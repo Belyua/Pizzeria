79debb627c5c8b8cc2f4b9b9de093579e9f473b0
Belyua <vanyab7777@gmail.com>
2022-12-16 20:31:18 +0100

    modify

--- Myshop/basket/basket.py
+++ Myshop/basket/basket.py
@@ -1,3 +1,4 @@
+from home.models import Product
 
 
 class Basket:
@@ -9,8 +10,12 @@ class Basket:
         self.basket = basket
 
     def add(self, product, qty):
-        product_id = str(product.id)
+        product_id = product.id
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+            self.basket[product_id] = {'price': product.price, 'qty': int(qty)}
 
         self.session.modified = True
+
+    # def __iter__(self):
+    #     product_ids = self.basket.keys()
+    #     products = Product.objects.filter(id_in=product_ids)
--- Myshop/basket/views.py
+++ Myshop/basket/views.py
@@ -13,8 +13,8 @@ def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        product_qty = product_id = int(request.POST.get('productqty'))
         basket.add(product=product, qty=product_qty)
-        response = JsonResponse({'qty': product_qty})
+        response = JsonResponse({'test': 'data'})
         return response
